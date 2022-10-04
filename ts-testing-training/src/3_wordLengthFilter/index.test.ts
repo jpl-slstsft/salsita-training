57107d4ccd7e0291d0587962ce967066b7df34fa
Jean-Philippe Leboeuf <jean-philippe@salsitasoft.com>
2022-10-04 16:26:11 +0200

    solve "3_wordLengthFilter"

--- ts-testing-training/src/3_wordLengthFilter/index.test.ts
+++ ts-testing-training/src/3_wordLengthFilter/index.test.ts
@@ -1,5 +1,7 @@
+import {wordLengthFilter} from '.';
+
 describe('wordLengthFilter', () => {
-  it('should do stuff', () => {
-    // put stuff here
+  it("should return ['quick', 'brown', 'jumps'] With input 'The quick brown fox jumps over the lazy dog', 4", () => {
+    expect(wordLengthFilter('The quick brown fox jumps over the lazy dog', 4)).toEqual(['quick', 'brown', 'jumps']);
   });
 });
--- ts-testing-training/src/3_wordLengthFilter/index.ts
+++ ts-testing-training/src/3_wordLengthFilter/index.ts
@@ -1,4 +1,11 @@
-// Write a function that takes a string and an integer n as parameters and
-// returns a list of all words that are longer than n.
-//
+// Write a function
+//  that takes a string and an integer n as parameters
+//  and returns a list of all words that are longer than n.
 // https://www.codewars.com/kata/5697fb83f41965761f000052
+
+export const wordLengthFilter = (str:string, len:number): string[] => {
+  const cleanStr = str.replace(/[^\w ]/, '');
+  const words = cleanStr.split(' ');
+  const wordsLengthFiltered = words.filter((word) => word.length > len);
+  return wordsLengthFiltered;
+};
