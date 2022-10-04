2c9a90a0260da6fe5ed97abbee3440b2dc822525
Jean-Philippe Leboeuf <jean-philippe@salsitasoft.com>
2022-10-04 16:28:32 +0200

    solve "4_isPrime"

--- ts-testing-training/src/4_isPrime/index.test.ts
+++ ts-testing-training/src/4_isPrime/index.test.ts
@@ -1,5 +1,28 @@
+import {isPrime} from '.';
+
 describe('isPrime', () => {
-  it('should return true if passed 0', () => {
-    // make me!
+  it('should return false if passed 0', () => {
+    expect(isPrime(0)).toEqual(false);
+  });
+  it('should return false if passed 1', () => {
+    expect(isPrime(1)).toEqual(false);
+  });
+  it('should return false if passed 2', () => {
+    expect(isPrime(2)).toEqual(true);
+  });
+  it('should return false if passed 3', () => {
+    expect(isPrime(3)).toEqual(true);
+  });
+  it('should return false if passed -1', () => {
+    expect(isPrime(-1)).toEqual(false);
+  });
+  it('should return true if passed the largest prime that can be stored in an unsigned 8-bit integer (251)', () => {
+    expect(isPrime(251)).toEqual(true);
+  });
+  it('should return true if passed the largest prime that can be stored in an unsigned 16-bit integer (65521)', () => {
+    expect(isPrime(65521)).toEqual(true);
+  });
+  it('should return true if passed the largest prime that can be stored in an signed 32-bit integer (2147483647)', () => {
+    expect(isPrime(2147483647)).toEqual(true);
   });
 });
--- ts-testing-training/src/4_isPrime/index.ts
+++ ts-testing-training/src/4_isPrime/index.ts
@@ -1,6 +1,19 @@
-// Make a function that takes a number and returns true if it's prime, and false
-// if it's not.  It should return false for 0.
+// Make a function
+//  that takes a number
+//  and returns true if it's prime, and false if it's not.
+//  It should return false for 0.
 
 export const isPrime = (input: number): boolean => {
-  return false;
+  if (input < 0) {
+    return false;
+  }
+  if ([0, 1].includes(input)) {
+    return false;
+  }
+  for (let i = 2; i < input; i++) {
+    if (input % i === 0) {
+      return false;
+    }
+  }
+  return true;
 };
