8aa3d5793886643e99c08f254fb5394a7544c005
Jean-Philippe Leboeuf <jean-philippe@salsitasoft.com>
2022-10-05 11:24:23 +0200

    solve "0_isEven"
    Cleaned up code written on Monday.

--- ts-testing-training/src/0_isEven/index.test.ts
+++ ts-testing-training/src/0_isEven/index.test.ts
@@ -1,5 +1,10 @@
+import {isEven} from ".";
+
 describe('isEven', () => {
   it('should return true if passed 0', () => {
-    // make me!
+    expect(isEven(0)).toEqual(true);
+  });
+  it('should return false if passed 3', () => {
+    expect(isEven(3)).toEqual(false);
   });
 });
--- ts-testing-training/src/0_isEven/index.ts
+++ ts-testing-training/src/0_isEven/index.ts
@@ -1,6 +1,7 @@
-// Make a function that takes a number and returns true if it's even, false if
-// not
+// Make a function
+//  that takes a number
+//  and returns true if it's even, false if not.
 
 export const isEven = (input: number): boolean => {
-  return false;
+  return input % 2 === 0;
 };
