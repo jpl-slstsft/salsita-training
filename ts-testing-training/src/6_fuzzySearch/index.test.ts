b7da130efa09b5b439032e70379e72c7e74e4829
Jean-Philippe Leboeuf <jean-philippe@salsitasoft.com>
2022-10-05 16:41:23 +0200

    solve "6_fuzzySearch"

--- ts-testing-training/src/6_fuzzySearch/index.test.ts
+++ ts-testing-training/src/6_fuzzySearch/index.test.ts
@@ -1,5 +1,34 @@
+import {fuzzySearch} from '.';
+
 describe('fuzzySearch', () => {
-  it('should do stuff', () => {
-    // put stuff here
+  it("should return the words 'apple' when called with 'a' and this word", () => {
+    expect(fuzzySearch('a', ['apple'])).toEqual(['apple']);
+  });
+  it("should return the words 'apple' when called with 'ap' and this word", () => {
+    expect(fuzzySearch('ap', ['apple'])).toEqual(['apple']);
+  });
+  it("should return the words 'apple' and 'grape' when called with 'a' and these words", () => {
+    expect(fuzzySearch('a', ['apple', 'grape'])).toEqual(['apple', 'grape']);
+  });
+  it("should return the words 'apple' and 'grape' when called with 'ap' and these words", () => {
+    expect(fuzzySearch('ap', ['apple', 'grape'])).toEqual(['apple', 'grape']);
+  });
+  it("should return the word 'apple' when called with 'al' and the words 'apple' and 'grape'", () => {
+    expect(fuzzySearch('al', ['apple', 'grape'])).toEqual(['apple']);
+  });
+  it("should return nothing when called with '' and the words 'apple' and 'grape'", () => {
+    expect(fuzzySearch('', ['apple', 'grape'])).toEqual([]);
+  });
+  it("should return the word 'apple' 2 times when called with 'ae' and the word 'apple' 2 times", () => {
+    expect(fuzzySearch('ae', ['apple', 'apple'])).toEqual(['apple', 'apple']);
+  });
+  it("should return nothing when called with 'az' and the word 'apple' 2 times", () => {
+    expect(fuzzySearch('az', ['apple', 'apple'])).toEqual([]);
+  });
+  it("should return nothing when called with 'z' and the word 'orange'", () => {
+    expect(fuzzySearch('z', ['orange'])).toEqual([]);
+  });
+  it("should return nothing when called with 'ez' and the word 'orange'", () => {
+    expect(fuzzySearch('ez', ['orange'])).toEqual([]);
   });
 });
--- ts-testing-training/src/6_fuzzySearch/index.ts
+++ ts-testing-training/src/6_fuzzySearch/index.ts
@@ -1,13 +1,34 @@
-// Write a function that takes a search term and a array of strings.  The
-// function should return all the elements in the array that fuzzy match the
-// search term.
-// A string fuzzy matches the search term if it contains the same letters as the
-// search term in the same order, even if there are gaps between the letters
+// Write a function that takes a search term and a array of strings.
+//  The function should return all the elements in the array
+//  that fuzzy match the search term.
+// A string fuzzy matches the search term
+//  if it contains the same letters as the search term in the same order,
+//  even if there are gaps between the letters.
 // For example:
-// - fuzzySearch('a', ['apple', 'grape']) // return ['apple', 'grape']
-//   - because 'a' is in 'apple' and 'grape'
-// - fuzzySearch('ap', ['apple', 'grape']) // return ['apple', 'grape']
-//   - because 'ap' is both 'apple' and 'grape'
-// - fuzzySearch('al', ['apple', 'grape']) // return ['apple']
-//   - because only apple contains the letters 'a' and 'l' in that order, even
-//     though there are gaps
+// - fuzzySearch('a', ['apple', 'grape'])  // returns ['apple', 'grape']
+//   - because 'a' is in 'apple' and 'grape';
+// - fuzzySearch('ap', ['apple', 'grape'])  // returns ['apple', 'grape']
+//   - because 'ap' is both 'apple' and 'grape';
+// - fuzzySearch('al', ['apple', 'grape'])  // returns ['apple']
+//   - because only 'apple' contains the letters 'a' and 'l' in that order,
+//      even though there are gaps.
+
+export const fuzzySearch = (q:string, str:string[]): string[] => {
+  const matches:string[] = [];
+  str.forEach((s:string) => {
+    let isMatch:boolean|undefined = undefined;
+    let pos:number = 0;
+    [...q].every((qc:string) => {
+      if ((pos = s.indexOf(qc, pos)) === -1) { // JS walrus "operator"
+        isMatch = false;
+        return false; // break
+      }
+      isMatch = true;
+      return true; // continue (explicit)
+    });
+    if (isMatch) {
+      matches.push(s);
+    }
+  });
+  return matches;
+};
