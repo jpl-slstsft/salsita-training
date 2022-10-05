40b1e6952ec7230dad2afbea71c8e743c1cdad87
Jean-Philippe Leboeuf <jean-philippe@salsitasoft.com>
2022-10-05 11:50:54 +0200

    solve "1_isAscending"
    Cleaned up and went on with code written on Monday.

--- ts-testing-training/src/1_isAscending/index.test.ts
+++ ts-testing-training/src/1_isAscending/index.test.ts
@@ -1,5 +1,25 @@
+import {isAscending} from ".";
+
 describe('isAscending', () => {
-  it('should do stuff', () => {
-    // put stuff here
+  it('should return true if passed numbers in ascending order', () => {
+    expect(isAscending([1, 2, 4, 7, 19])).toEqual(true);
+  });
+  it('should return true if passed numbers in ascending order that follow one another', () => {
+    expect(isAscending([1, 2, 3, 4, 5])).toEqual(true);
+  });
+  it('should return false if passed numbers like: one ascending series, then another ascending one but starting lower', () => {
+    expect(isAscending([1, 6, 10, 18, 2, 4, 20])).toEqual(false);
+  });
+  it('should return false if passed numbers in descending order that follow one another', () => {
+    expect(isAscending([9, 8, 7, 6, 5, 4, 3, 2, 1])).toEqual(false);
+  });
+  it('should return true if passed no numbers', () => {
+    expect(isAscending([])).toEqual(true);
+  });
+  it('should return true if passed only one number', () => {
+    expect(isAscending([1])).toEqual(true);
+  });
+  it('should return false if passed numbers like: 1 2 3 2', () => {
+    expect(isAscending([1, 2, 3, 2])).toEqual(false);
   });
 });
--- ts-testing-training/src/1_isAscending/index.ts
+++ ts-testing-training/src/1_isAscending/index.ts
@@ -1,1 +1,13 @@
-// See https://www.codewars.com/kata/56b7f2f3f18876033f000307 for prompt
+// See https://www.codewars.com/kata/56b7f2f3f18876033f000307 for prompt.
+
+export const isAscending = (arr:number[]): boolean => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      // Edge cases if we would put arr.length instead of arr.length-1:
+      //  + No need for a specific case when there is only one number, because:
+      //     ( 1 > undefined )→ false ( also ( 1 < undefined )→ false ).
+      return false;
+    }
+  }
+  return true;
+};
