47240773d4bf7c92bd81a093c10769148a371b32
Jean-Philippe Leboeuf <jean-philippe@salsitasoft.com>
2022-10-05 12:03:48 +0200

    solve "2_numberStack"
    Cleaned up code written on Tuesday.

--- ts-testing-training/src/2_numberStack/index.test.ts
+++ ts-testing-training/src/2_numberStack/index.test.ts
@@ -1,5 +1,68 @@
+import {NumberStack} from '.';
+
 describe('Stack', () => {
-  it('should do stuff', () => {
-    // do stuff!
+  it('should be empty when instantiated', () => {
+    const aNumberStack = new NumberStack();
+    expect(aNumberStack.contents).toEqual([]);
+  });
+  describe('push', () => {
+    let aNumberStack:NumberStack;
+    beforeEach(() => {
+      aNumberStack = new NumberStack();
+    });
+    it('should make the content of the stack [1] when the stack is instantiated then 1 is pushed', () => {
+      aNumberStack.push(1);
+      expect(aNumberStack.contents).toEqual([1]);
+    });
+    it('should make the content of the stack [1, 2] when the stack is instantiated then 1 then 2 are pushed', () => {
+      aNumberStack.push(1);
+      aNumberStack.push(2);
+      expect(aNumberStack.contents).toEqual([1, 2]);
+    });
+  });
+  describe('top', () => {
+    it('should return undefined when the stack is instantiated', () => {
+      const aNumberStack = new NumberStack();
+      expect(aNumberStack.top).toBeUndefined();
+    });
+    it('should return 1 when the stack is instantiated and 1 is pushed', () => {
+      const aNumberStack = new NumberStack();
+      aNumberStack.push(1);
+      expect(aNumberStack.top).toEqual(1);
+    });
+    it('should return 2 when the stack is instantiated and 1 then 2 are pushed', () => {
+      const aNumberStack = new NumberStack();
+      aNumberStack.push(1);
+      aNumberStack.push(2);
+      expect(aNumberStack.top).toEqual(2);
+    });
+  });
+  describe('pop', () => {
+    it('should throw "empty stack" when the stack is empty', () => {
+      const aNumberStack = new NumberStack();
+      const t = () => {
+        aNumberStack.pop();
+      };
+      expect(t).toThrow('EmptyStack');
+    });
+    describe('when the stack contains [1]', () => {
+      let aNumberStack:NumberStack;
+      beforeEach(() => {
+        aNumberStack = new NumberStack();
+        aNumberStack.push(1);
+      });
+      it('should return 1', () => {
+        expect(aNumberStack.pop()).toEqual(1);
+      });
+      it('should make the stack empty', () => {
+        aNumberStack.pop();
+        expect(aNumberStack.contents.length).toEqual(0);
+      });
+      it('should make the stack [1] if 2 has also been pushed', () => {
+        aNumberStack.push(2);
+        aNumberStack.pop();
+        expect(aNumberStack.contents).toEqual([1]);
+      });
+    });
   });
 });
--- ts-testing-training/src/2_numberStack/index.ts
+++ ts-testing-training/src/2_numberStack/index.ts
@@ -1,24 +1,24 @@
 // Create a class that creates a number stack object.
-// The stack should behave like a classic LIFO, where the last number you put in
-// is the first one you get out.
-// The skeleton for this class has been made for you.  Build out each function
-// using test driven development.
+// The stack should behave like a classic LIFO,
+//  where the last number you put in is the first one you get out.
+// The skeleton for this class has been made for you.
+//  Build out each function using test driven development.
 // Remember the process -
 // - RED:
-//   - Write a single test for a small bit of functionality (Don't start writing
-//     a test for final, most complex part of the code.  Go slowly, you'll get
-//     there)
-//   - The test will fail (that's normal)
+//   - Write a single test for a small bit of functionality
+//      (Don't start writing a test for final, most complex part of the code.
+//      Go slowly, you'll get there).
+//   - The test will fail (that's normal).
 // - GREEN:
-//   - Write the smallest amount of code possible JUST TO MAKE YOUR CURRENT
-//     TESTS PASS
-//   - Don't worry about having the full functionality yet.  Just make your
-//     current tests green.
+//   - Write the smallest amount of code possible
+//      JUST TO MAKE YOUR CURRENT TESTS PASS.
+//   - Don't worry about having the full functionality yet.
+//      Just make your current tests green.
 // - REFACTOR:
-//   - Refactor any of your existing code to make it cleaner, you will now know
-//     if your changes break any functionality
+//   - Refactor any of your existing code to make it cleaner,
+//      you will now know if your changes break any functionality.
 // - REPEAT:
-//   - Start over again from RED, adding another small bit of functionality
+//   - Start over again from RED, adding another small bit of functionality.
 
 /**
  * A LIFO stack that can contain numbers
@@ -27,8 +27,8 @@ export class NumberStack {
   private list: number[];
 
   /**
-   * Sets up a new NumberStack object.  Is called whenever `new NumberStack()`
-   * is called.
+   * Sets up a new NumberStack object.
+   *  Is called whenever `new NumberStack()` is called.
    */
   constructor() {
     this.list = [];
@@ -36,24 +36,27 @@ export class NumberStack {
 
   /** Puts a number on the top of the stack */
   public push(input: number): void {
-    return;
+    this.list.push(input);
   }
 
   /** Remove the most recently pushed number from the stack and return it */
   public pop(): number {
-    return 0;
+    if (this.list.length === 0) throw new Error('EmptyStack');
+    const popped = this.list.pop();
+    if (typeof popped === 'undefined') throw new Error('EmptyStack');
+    return popped;
   }
 
   /**
-   * Returns the most recently pushed number in the stack.  Does not modify the
-   * contents of the stack itself
+   * Returns the most recently pushed number in the stack.
+   *  Does not modify the contents of the stack itself.
    */
-  get top(): number {
-    return 0;
+  get top(): number|undefined {
+    return this.contents.at(-1);
   }
 
   /** Returns all the contents of the stack */
   get contents(): number[] {
-    return [];
+    return this.list;
   }
 }
